e81afcfd5e6a248c8579c834367cc00d46e1f36d
Semih Orhan <sorhan3.14@gmail.com>
2025-03-05 17:39:25 +0300

    eslint prettier setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Welcome!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline isolate">
+      Welcome to Code Genius!
+    </h1>
+  );
 }
